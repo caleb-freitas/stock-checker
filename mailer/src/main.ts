c1349a57802b52f3ca62af876fbd529a9530a1b0
caleb-freitas <calebfreitas@tutanota.com>
2022-06-07 02:11:14 -0300

    chore: added kafka setup

--- mailer/src/main.ts
+++ mailer/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        clientId: 'mailer',
+        brokers: ['host.docker.internal:9094'],
+      },
+    },
+  });
+  app.startAllMicroservices().then(() => {
+    console.log('[Mailer] microservice running');
+  });
+  app.listen(3000).then(() => {
+    console.log('[Mailer] HTTP server running');
+  });
 }
 bootstrap();
