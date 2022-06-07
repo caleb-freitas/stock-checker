f8fb4e40e132832c3c47d8d58bfb539e752ef6ca
caleb-freitas <calebfreitas@tutanota.com>
2022-06-06 23:17:40 -0300

    fix: added check controller to module declaration

--- checker/src/http/http.module.ts
+++ checker/src/http/http.module.ts
@@ -3,9 +3,11 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from '../database/database.module';
 import { MessagingModule } from '../messaging/messaging.module';
 import { CheckerService } from '../services/checker.service';
+import { CheckerController } from './checker.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, MessagingModule],
   providers: [CheckerService],
+  controllers: [CheckerController],
 })
 export class HttpModule {}
